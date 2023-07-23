2e7fe50a6759425ab9cdfc03567e862fc122bcea
alexchikov <alex.chikov@outlook.com>
2023-07-24 01:30:49 +0300

    formatted code

--- database.py
+++ database.py
@@ -5,55 +5,58 @@ import config
 
 Base = declarative_base()
 
+
 class Items(Base):
     __tablename__ = 'items'
-    
-    id = Column('id', 
-                Integer, 
+
+    id = Column('id',
+                Integer,
                 primary_key=True,
                 nullable=False,
                 autoincrement=True,
                 unique=True)
-    
+
     name = Column('name',
                   String(80),
                   nullable=False)
-    
+
     price = Column('price',
                    Integer,
                    nullable=False)
-    
+
+
 class Orders(Base):
-    
+
     __tablename__ = 'orders'
-    
-    id = Column('id', 
-                Integer, 
+
+    id = Column('id',
+                Integer,
                 primary_key=True,
                 nullable=False,
                 autoincrement=True,
                 unique=True)
-    
-    datetime = Column('datetime', 
-                      DateTime, 
+
+    datetime = Column('datetime',
+                      DateTime,
                       nullable=False)
-    
+
     customer_name = Column('customer_name',
                            String(120),
                            nullable=False)
-    
+
     city = Column('city', String(80))
     street = Column('street', String(80))
-    
+
     ordered_items = ...
-    
+
     total_price = Column('total_price',
                          Integer,
                          nullable=False)
-    
+
     delivery_instructions = Column('delivery_instructions',
                                    String(200))
-    
-    
-engine = create_engine(f"postgresql+psycopg2://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}")
+
+
+engine = create_engine(
+    f"postgresql+psycopg2://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}")
 Base.metadata.create_all(engine)
--- generator.py
+++ generator.py
@@ -6,27 +6,31 @@ import json
 from time import sleep
 from datetime import datetime
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(levelname)s (%(asctime)s): %(message)s")
+logging.basicConfig(level=logging.INFO, stream=sys.stdout,
+                    format="%(levelname)s (%(asctime)s): %(message)s")
+
 
 def generate_items(number: int, items: list) -> dict:
     order_items, summa = list(), int()
     for item in range(number):
-        id = random.randint(1,20)
-        quantity = random.randint(1,10)
+        id = random.randint(1, 20)
+        quantity = random.randint(1, 10)
         order_items.append({"item_id": items[id-1]["item_id"],
                             "quantity": quantity
                             })
-        summa += round(items[id-1]["price"]*quantity,2)
+        summa += round(items[id-1]["price"]*quantity, 2)
     return (order_items, summa)
-        
+
+
 def generate_order(current_id: id) -> dict:
     customer_gender = random.choice(["male", "female"])
-    customer_name = random.choice(samples.name_samples[customer_gender])+" "+\
+    customer_name = random.choice(samples.name_samples[customer_gender])+" " +\
         random.choice(samples.surname_samples[customer_gender])
-    
-    customer_address = " ".join([val for val in random.choice(samples.addresses).values()])
+
+    customer_address = " ".join(
+        [val for val in random.choice(samples.addresses).values()])
     with open("items.json") as file:
-        customer_items = generate_items(random.randint(1,10), json.load(file))
+        customer_items = generate_items(random.randint(1, 10), json.load(file))
     return {"order_id": current_id,
             "date": datetime.now().strftime("%d.%m.%Y"),
             "time": datetime.now().strftime("%H%M%S"),
@@ -35,8 +39,9 @@ def generate_order(current_id: id) -> dict:
             "ordered_items": customer_items[0],
             "total_price": customer_items[1],
             "delivery_instructions": "Please ring doorbell when delivering."
-}
-    
+            }
+
+
 if __name__ == "__main__":
     id = int()
     while True:
@@ -44,5 +49,6 @@ if __name__ == "__main__":
         order = generate_order(id)
         logging.info(f"Successfully generated order {id}")
         with open(f"orders/order_{id}_{datetime.now().strftime('%d-%m_%H:%M')}.json", "w") as file:
-            json.dump(order, file, indent=4, ensure_ascii=False, separators=[",", ":"])
+            json.dump(order, file, indent=4,
+                      ensure_ascii=False, separators=[",", ":"])
         sleep(random.randint(30, 1800))
